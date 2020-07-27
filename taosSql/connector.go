1a3b1941c206a654ece25c05570d111d727455ce
yihaoDeng <luomoxyz@126.com>
2020-07-27 18:26:16 +0000

    add default

--- taosSql/connector.go
+++ taosSql/connector.go
@@ -35,10 +35,17 @@ func (c *connector) Connect(ctx context.Context) (driver.Conn, error) {
 	}
 
 	// Connect to Server
+	if len(mc.cfg.user) == 0 {
+		mc.cfg.user = "root"
+	}
+	if len(mc.cfg.passwd) == 0 {
+		mc.cfg.passwd = "taosdata"
+	}
 	mc.taos, err = mc.taosConnect(mc.cfg.addr, mc.cfg.user, mc.cfg.passwd, mc.cfg.dbName, mc.cfg.port)
 	if err != nil {
 		return nil, err
 	}
+
 	if len(mc.cfg.dbName) != 0 {
 		_, err := mc.Exec(strings.Join([]string{"use", mc.cfg.dbName}, " "), nil)
 		if err != nil {
