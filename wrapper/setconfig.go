a078dbbf75720916310e3a485172bbcf167cb948
t_max <1172915550@qq.com>
2021-09-01 14:20:07 +0800

    [TD-5991]<enhance>: add  test

--- wrapper/setconfig.go
+++ wrapper/setconfig.go
@@ -1,12 +1,21 @@
 package wrapper
 
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <taos.h>
+*/
 import "C"
-import "encoding/json"
+import (
+	"encoding/json"
+	"unsafe"
+)
 
 // TaosSetConfig int   taos_set_config(const char *config);
 func TaosSetConfig(params map[string]string) int {
 	config, _ := json.Marshal(params)
 	cConfig := C.CString(string(config))
-	defer C.free(cConfig)
+	defer C.free(unsafe.Pointer(cConfig))
 	return int(C.taos_set_config(cConfig))
 }
