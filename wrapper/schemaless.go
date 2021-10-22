03bb150d897981c018cfdb28957468ac4a57aaa7
t_max <1172915550@qq.com>
2021-10-22 17:30:02 +0800

    [TD-10425]<fix>: fix schemaless api

--- wrapper/schemaless.go
+++ wrapper/schemaless.go
@@ -10,13 +10,22 @@ import "C"
 import "unsafe"
 
 const (
-	InfluxDBLineProtocol       = 0
-	OpenTSDBTelnetLineProtocol = 1
-	OpenTSDBJsonFormatProtocol = 2
+	InfluxDBLineProtocol       = 1
+	OpenTSDBTelnetLineProtocol = 2
+	OpenTSDBJsonFormatProtocol = 3
+)
+const (
+	TSDB_SML_TIMESTAMP_NOT_CONFIGURED = iota
+	TSDB_SML_TIMESTAMP_HOURS
+	TSDB_SML_TIMESTAMP_MINUTES
+	TSDB_SML_TIMESTAMP_SECONDS
+	TSDB_SML_TIMESTAMP_MILLI_SECONDS
+	TSDB_SML_TIMESTAMP_MICRO_SECONDS
+	TSDB_SML_TIMESTAMP_NANO_SECONDS
 )
 
-// TaosSchemalessInsert DLL_EXPORT int taos_schemaless_insert(TAOS* taos, char* lines[], int numLines, int protocol, char* precision);
-func TaosSchemalessInsert(taosConnect unsafe.Pointer, lines []string, protocol int, precision string) int {
+// TaosSchemalessInsert TAOS_RES *taos_schemaless_insert(TAOS* taos, char* lines[], int numLines, int protocol, int precision);
+func TaosSchemalessInsert(taosConnect unsafe.Pointer, lines []string, protocol int, precision string) unsafe.Pointer {
 	numLines := len(lines)
 	var cLines = make([]*C.char, numLines)
 	needFreeList := make([]unsafe.Pointer, numLines)
@@ -31,10 +40,28 @@ func TaosSchemalessInsert(taosConnect unsafe.Pointer, lines []string, protocol i
 		cLines[i] = cLine
 	}
 	if len(precision) == 0 {
-		return int(C.taos_schemaless_insert(taosConnect, (**C.char)(&cLines[0]), (C.int)(numLines), (C.int)(protocol), nil))
+		return C.taos_schemaless_insert(taosConnect, (**C.char)(&cLines[0]), (C.int)(numLines), (C.int)(protocol), (C.int)(TSDB_SML_TIMESTAMP_NOT_CONFIGURED))
 	} else {
-		cPrecision := C.CString(precision)
-		defer C.free(unsafe.Pointer(cPrecision))
-		return int(C.taos_schemaless_insert(taosConnect, (**C.char)(&cLines[0]), (C.int)(numLines), (C.int)(protocol), cPrecision))
+		return C.taos_schemaless_insert(taosConnect, (**C.char)(&cLines[0]), (C.int)(numLines), (C.int)(protocol), (C.int)(exchange(precision)))
 	}
+}
+
+func exchange(ts string) int {
+	switch ts {
+	case "":
+		return TSDB_SML_TIMESTAMP_NOT_CONFIGURED
+	case "h":
+		return TSDB_SML_TIMESTAMP_HOURS
+	case "m":
+		return TSDB_SML_TIMESTAMP_MINUTES
+	case "s":
+		return TSDB_SML_TIMESTAMP_SECONDS
+	case "ms":
+		return TSDB_SML_TIMESTAMP_MILLI_SECONDS
+	case "u", "μ":
+		return TSDB_SML_TIMESTAMP_MICRO_SECONDS
+	case "ns":
+		return TSDB_SML_TIMESTAMP_NANO_SECONDS
+	}
+	return TSDB_SML_TIMESTAMP_NOT_CONFIGURED
 }
