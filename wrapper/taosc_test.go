0b3e8a9093469f240ad3c29c5bafaefaf2025cf5
t_max <1172915550@qq.com>
2021-11-05 13:16:48 +0800

    [TD-10879]<fix>: fix test

--- wrapper/taosc_test.go
+++ wrapper/taosc_test.go
@@ -6,36 +6,37 @@ import (
 	"testing"
 	"unsafe"
 
+	"github.com/taosdata/driver-go/v2/common"
 	"github.com/taosdata/driver-go/v2/wrapper/cgo"
 )
 
-//func TestTaosOptions(t *testing.T) {
-//	type args struct {
-//		option int
-//		value  string
-//	}
-//	tests := []struct {
-//		name string
-//		args args
-//		want int
-//	}{
-//		{
-//			name: "test_options",
-//			args: args{
-//				option: common.TSDB_OPTION_CONFIGDIR,
-//				value:  "/etc/taos",
-//			},
-//			want: 0,
-//		},
-//	}
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			if got := TaosOptions(tt.args.option, tt.args.value); got != tt.want {
-//				t.Errorf("TaosOptions() = %v, want %v", got, tt.want)
-//			}
-//		})
-//	}
-//}
+func TestTaosOptions(t *testing.T) {
+	type args struct {
+		option int
+		value  string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "test_options",
+			args: args{
+				option: common.TSDB_OPTION_CONFIGDIR,
+				value:  "/etc/taos",
+			},
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := TaosOptions(tt.args.option, tt.args.value); got != tt.want {
+				t.Errorf("TaosOptions() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
 
 type result struct {
 	res unsafe.Pointer
