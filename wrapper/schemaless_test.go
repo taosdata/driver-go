07fabd6f5ada31ee0be51ea12cd21767cb22ea25
t_max <1172915550@qq.com>
2021-11-16 10:23:18 +0800

    [TD-11064]<test>: add schemaless function test and benchmark

---
+++ wrapper/schemaless_test.go
@@ -0,0 +1,76 @@
+package wrapper_test
+
+import (
+	"testing"
+	"time"
+	"unsafe"
+
+	"github.com/taosdata/driver-go/v2/errors"
+	"github.com/taosdata/driver-go/v2/wrapper"
+)
+
+var conn unsafe.Pointer
+
+func TestMain(m *testing.M) {
+	var err error
+	conn, err = wrapper.TaosConnect("", "root", "taosdata", "log", 0)
+	if err != nil {
+		panic(err)
+		return
+	}
+	m.Run()
+	defer wrapper.TaosClose(conn)
+}
+
+func BenchmarkTelnetSchemaless(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		result := wrapper.TaosSchemalessInsert(conn, []string{
+			"sys_if_bytes_out 1636626444 1.3E3 host=web01 interface=eth0",
+		}, wrapper.OpenTSDBTelnetLineProtocol, "")
+		code := wrapper.TaosError(result)
+		if code != 0 {
+			errStr := wrapper.TaosErrorStr(result)
+			wrapper.TaosFreeResult(result)
+			b.Error(&errors.TaosError{
+				Code:   int32(code) & 0xffff,
+				ErrStr: errStr,
+			})
+			return
+		}
+		wrapper.TaosFreeResult(result)
+	}
+}
+
+// Just check if C api is available
+func TestSchemalessTelnet(t *testing.T) {
+	result := wrapper.TaosSchemalessInsert(conn, []string{
+		"sys_if_bytes_out 1636626444 1.3E3 host=web01 interface=eth0",
+	}, wrapper.OpenTSDBTelnetLineProtocol, "")
+	code := wrapper.TaosError(result)
+	if code != 0 {
+		errStr := wrapper.TaosErrorStr(result)
+		wrapper.TaosFreeResult(result)
+		t.Error(&errors.TaosError{
+			Code:   int32(code) & 0xffff,
+			ErrStr: errStr,
+		})
+		return
+	}
+	wrapper.TaosFreeResult(result)
+	s := time.Now()
+	result = wrapper.TaosSchemalessInsert(conn, []string{
+		"sys_if_bytes_out 1636626444 1.3E3 host=web01 interface=eth0",
+	}, wrapper.OpenTSDBTelnetLineProtocol, "")
+	code = wrapper.TaosError(result)
+	if code != 0 {
+		errStr := wrapper.TaosErrorStr(result)
+		wrapper.TaosFreeResult(result)
+		t.Error(&errors.TaosError{
+			Code:   int32(code) & 0xffff,
+			ErrStr: errStr,
+		})
+		return
+	}
+	wrapper.TaosFreeResult(result)
+	t.Log("finish ", time.Now().Sub(s))
+}
