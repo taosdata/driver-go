10b56c1d360c91db33b452d01b9734f8f60378e1
t_max <1172915550@qq.com>
2022-06-30 11:07:30 +0800

    fix: update taos_set_config

--- wrapper/setconfig.go
+++ wrapper/setconfig.go
@@ -7,27 +7,36 @@ package wrapper
 #include <taos.h>
 */
 import "C"
+import (
+	"strings"
+	"unsafe"
+
+	"github.com/taosdata/driver-go/v2/errors"
+)
 
 // TaosSetConfig int   taos_set_config(const char *config);
 func TaosSetConfig(params map[string]string) error {
-	return nil
-	// danger!! taos_set_config must set params. if set nil or empty map will get error
-	//if len(params) == 0 {
-	//	return nil
-	//}
-	//config, _ := json.Marshal(params)
-	//cConfig := C.CString(string(config))
-	//defer C.free(unsafe.Pointer(cConfig))
-	//result := (C.struct_setConfRet)(C.taos_set_config(cConfig))
-	//if int(result.retCode) == -5 || int(result.retCode) == 0 {
-	//	return nil
-	//}
-	//buf := &strings.Builder{}
-	//for _, c := range result.retMsg {
-	//	if c == 0 {
-	//		break
-	//	}
-	//	buf.WriteByte(byte(c))
-	//}
-	//return &errors.TaosError{Code: int32(result.retCode) & 0xffff, ErrStr: buf.String()}
+	if len(params) == 0 {
+		return nil
+	}
+	buf := &strings.Builder{}
+	for k, v := range params {
+		buf.WriteString(k)
+		buf.WriteString(" ")
+		buf.WriteString(v)
+	}
+	cConfig := C.CString(buf.String())
+	defer C.free(unsafe.Pointer(cConfig))
+	result := (C.struct_setConfRet)(C.taos_set_config(cConfig))
+	if int(result.retCode) == -5 || int(result.retCode) == 0 {
+		return nil
+	}
+	buf.Reset()
+	for _, c := range result.retMsg {
+		if c == 0 {
+			break
+		}
+		buf.WriteByte(byte(c))
+	}
+	return errors.NewError(result.retCode, buf.String())
 }
