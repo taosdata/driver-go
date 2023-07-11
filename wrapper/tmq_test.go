d5d40266e5baf594dd1c77a093be7f69523468f1
t_max <1172915550@qq.com>
2023-07-11 10:48:00 +0800

    fix: get offset after seek

--- wrapper/tmq_test.go
+++ wrapper/tmq_test.go
@@ -1525,7 +1525,7 @@ func TestTMQGetTopicAssignment(t *testing.T) {
 	}
 	assert.Equal(t, 1, len(assignment))
 	assert.Equal(t, int64(0), assignment[0].Begin)
-	assert.Equal(t, int64(1), assignment[0].Offset)
+	assert.Equal(t, int64(0), assignment[0].Offset)
 	assert.GreaterOrEqual(t, assignment[0].End, end)
 	end = assignment[0].End
 	assert.Equal(t, int32(vgID), assignment[0].VGroupID)
