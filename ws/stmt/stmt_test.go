2d2a158ec1d5ddf78bce041caf81790f88cbd15a
t_max <1172915550@qq.com>
2024-01-19 14:22:03 +0800

    enh: adapts to new websocket interface

--- ws/stmt/stmt_test.go
+++ ws/stmt/stmt_test.go
@@ -158,7 +158,7 @@ func TestStmt(t *testing.T) {
 	}
 	defer cleanEnv()
 	now := time.Now()
-	config := NewConfig("ws://127.0.0.1:6041/rest/stmt", 0)
+	config := NewConfig("ws://127.0.0.1:6041", 0)
 	config.SetConnectUser("root")
 	config.SetConnectPass("taosdata")
 	config.SetConnectDB("test_ws_stmt")
