5b887c7017a59a40dc7bf45e0fa55a7d9e6225f8
t_max <1172915550@qq.com>
2024-08-29 13:59:34 +0800

    chore: merge 3.1 to 3.0

--- taosRestful/connection.go
+++ taosRestful/connection.go
@@ -3,6 +3,7 @@ package taosRestful
 import (
 	"compress/gzip"
 	"context"
+	"crypto/tls"
 	"database/sql/driver"
 	"encoding/base64"
 	"errors"
@@ -36,18 +37,24 @@ func newTaosConn(cfg *config) (*taosConn, error) {
 		readBufferSize = 4 << 10
 	}
 	tc := &taosConn{cfg: cfg, readBufferSize: readBufferSize}
+	transport := &http.Transport{
+		Proxy: http.ProxyFromEnvironment,
+		DialContext: (&net.Dialer{
+			Timeout:   30 * time.Second,
+			KeepAlive: 30 * time.Second,
+		}).DialContext,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+		DisableCompression:    cfg.disableCompression,
+	}
+	if cfg.skipVerify {
+		transport.TLSClientConfig = &tls.Config{
+			InsecureSkipVerify: true,
+		}
+	}
 	tc.client = &http.Client{
-		Transport: &http.Transport{
-			Proxy: http.ProxyFromEnvironment,
-			DialContext: (&net.Dialer{
-				Timeout:   30 * time.Second,
-				KeepAlive: 30 * time.Second,
-			}).DialContext,
-			IdleConnTimeout:       90 * time.Second,
-			TLSHandshakeTimeout:   10 * time.Second,
-			ExpectContinueTimeout: 1 * time.Second,
-			DisableCompression:    cfg.disableCompression,
-		},
+		Transport: transport,
 	}
 	path := "/rest/sql"
 	if len(cfg.dbName) != 0 {
--- taosRestful/dsn_test.go
+++ taosRestful/dsn_test.go
@@ -10,15 +10,16 @@ import (
 // @description: test parse dsn
 func TestParseDsn(t *testing.T) {
 	tcs := []struct {
-		dsn    string
-		errs   string
-		user   string
-		passwd string
-		net    string
-		addr   string
-		port   int
-		dbName string
-		token  string
+		dsn        string
+		errs       string
+		user       string
+		passwd     string
+		net        string
+		addr       string
+		port       int
+		dbName     string
+		token      string
+		skipVerify bool
 	}{{},
 		{dsn: "abcd", errs: "invalid DSN: missing the slash separating the database name"},
 		{dsn: "user:passwd@http(fqdn:6041)/dbname", user: "user", passwd: "passwd", net: "http", addr: "fqdn", port: 6041, dbName: "dbname"},
@@ -28,6 +29,7 @@ func TestParseDsn(t *testing.T) {
 		{dsn: "user:passwd@https(:0)/", user: "user", passwd: "passwd", net: "https"},
 		{dsn: "user:passwd@https(:0)/?interpolateParams=false&test=1", user: "user", passwd: "passwd", net: "https"},
 		{dsn: "user:passwd@https(:0)/?interpolateParams=false&token=token", user: "user", passwd: "passwd", net: "https", token: "token"},
+		{dsn: "user:passwd@https(:0)/?interpolateParams=false&token=token&skipVerify=true", user: "user", passwd: "passwd", net: "https", token: "token", skipVerify: true},
 	}
 	for i, tc := range tcs {
 		name := fmt.Sprintf("%d - %s", i, tc.dsn)
@@ -45,7 +47,9 @@ func TestParseDsn(t *testing.T) {
 				cfg.passwd != tc.passwd ||
 				cfg.net != tc.net ||
 				cfg.addr != tc.addr ||
-				cfg.port != tc.port {
+				cfg.port != tc.port ||
+				cfg.token != tc.token ||
+				cfg.skipVerify != tc.skipVerify {
 				t.Fatal(cfg)
 			}
 		})
